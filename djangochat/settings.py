3653122b6fe64bcfafa4c9d8c495251cc05ff57d
imraan024 <asq24i@gmail.com>
2021-07-10 03:57:03 +0600

    move to postgresql

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'djangochat.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':  {
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'chatApp',
+        'USER' : 'postgres',
+        'PASSWORD' : '12345',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5432',
+	}
 }
 
 
